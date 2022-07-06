cc3d6b0588034a662cb0140ae0bb1d7f2704c9d3
Adhish Basak <adhishbasak98@gmail.com>
2022-07-06 12:20:15 +0530

    BookSchema Added

---
+++ src/models/bookModel.js
@@ -0,0 +1,49 @@
+const mongoose = require ('mongoose')
+
+const bookSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required: true,
+        unique : true
+    },
+    excerpt:{
+        type : String,
+        required: true,
+   },
+   userId: {
+    type : mongoose.Types.ObjectId,
+    required: true,
+    ref: "User"
+  },
+  ISBN: {
+    type: String, 
+    required: true,
+    unique: true
+},
+  category: {
+     type: String,
+     required: true
+},
+  subcategory: {
+    type: [string],
+    required: true
+},
+  reviews: {
+    type: Number,
+    default: 0
+    //comment: Holds number of reviews of this book
+},
+  deletedAt: Date, 
+  isDeleted: {
+     type: Boolean,
+     default: false
+},
+  releasedAt: {
+    type: Date,      //format("YYYY-MM-DD")
+    required: true,
+}
+
+},{timestamps: true})
+
+
+module.exports = mongoose.model("Book", bookSchema);
