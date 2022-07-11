fc47e8d3c5dcecfaf5574595d9e8b22e6e810f45
Adhish Basak <adhishbasak98@gmail.com>
2022-07-11 17:11:21 +0530

    Auth and Delete Review Modified.

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -79,7 +79,11 @@ const deleteReview = async (req, res) => {
   try {
     let bookId = req.params.bookId
     let reviewId = req.params.reviewId
-    let book = await bookModel.findOne({ _id: bookId, isDeleted: false }).select({ _id: 1, reviews: 1 })
+
+    if (!ObjectId.isValid(bookId)) return res.status(400).send({status : false, message : "Enter Valid Book Id."})
+    if (!ObjectId.isValid(reviewId)) return res.status(400).send({status : false, message : "Enter Valid Review Id."})
+   
+    let book = await bookModel.findOne({ _id: bookId, isDeleted: false })
 
     if (!book) return res.status(404).send({ status: true, message: "Book Does Not Found !!!" })
     let reviews = book.reviews
