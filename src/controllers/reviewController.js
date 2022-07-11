e39f85bdb1fb59d04662a778b02311d959727fb0
pushpak <pushpak99311@gmail.com>
2022-07-11 17:24:23 +0530

    save updated

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const bookModel = require("../models/bookModel");
 const ObjectId = mongoose.Types.ObjectId;
 
+/*############################################ CREATE REVIEWS FOR BOOK ##########################################################*/
+
 const createReviewForBook = async (req, res) => {
   try {
     const bookId = req.params.bookId;
@@ -39,4 +41,90 @@ const createReviewForBook = async (req, res) => {
   }
 };
 
-module.exports.createReviewForBook = createReviewForBook;
+/*############################################ UPDATE REVIEWS FOR BOOK ##########################################################*/
+
+const updateReviewForBook = async (req, res) => {
+try{
+  let bookId = req.params.bookId
+  if (!ObjectId.isValid(bookId)) {
+    return res.status(400).send({ status: false, msg: "Book Id is Invalid" });
+  }
+
+  let reviewId = req.params.reviewId
+  if (!ObjectId.isValid(reviewId)) {
+    return res.status(400).send({ status: false, msg: "Review Id is Invalid" });
+  }
+
+  let reviewDetail = req.body
+  let {review, rating, reviewedBy} = reviewDetail
+  
+  let obj = {}
+  
+  
+  obj.review = review
+  obj.rating = rating
+  obj.reviewedBy = reviewedBy
+
+ 
+  let checkByBook = await bookModel.findOne({_id: bookId, isDeleted: false})
+  if(!checkByBook){
+    return res.status(404).send({ status: true, message: "Book Does Not Found !!!" })
+  }
+
+  obj.reviewsData = checkByBook.reviews
+
+
+  let checkByBookId = checkByBook._id.toString()
+
+  let updatedReviewDetail = await reviewModel.findOneAndUpdate(
+    { _id: reviewId, bookId: checkByBookId, isDeleted: false },
+    {checkByBook, obj},
+    { returnDocument: "after" }
+  )
+  if(updatedReviewDetail == null) {
+    return res.status(404).send({ status: true, message: "Update request denied, review is no more available" })
+  }
+  
+
+  console.log(updatedReviewDetail)
+  return res.status(200).send({ status: true, message: "Success", data: updatedReviewDetail })
+}
+catch(err){
+  return res.status(500).send({ status: false, message: err.message })
+}
+}
+
+
+/*############################################ DELETE REVIEWS FOR BOOK ##########################################################*/
+
+const deleteReview = async (req, res) => {
+  try {
+    let bookId = req.params.bookId
+    let reviewId = req.params.reviewId
+    let book = await bookModel.findOne({ _id: bookId, isDeleted: false }).select({ _id: 1 })
+
+    if (!book) return res.status(404).send({ status: true, message: "Book Does Not Found !!!" })
+    book = book._id.toString()
+
+    let review = await reviewModel.findOneAndUpdate(
+      { _id: reviewId, bookId: bookId, isDeleted: false },
+      { $set: { isDeleted: true } }
+    )
+
+    if (!review) return res.status(404).send({ status: true, message: "Review Does Not Found !!!" })
+
+    res.status(200).send({ status: true, message: "Review Deleted !!!" })
+
+
+  } catch (err) {
+    res.status(500).send({ status: false, message: err.message });
+  }
+}
+
+
+
+
+module.exports.createReviewForBook = createReviewForBook
+module.exports.updateReviewForBook = updateReviewForBook
+module.exports.deleteReview = deleteReview
+
